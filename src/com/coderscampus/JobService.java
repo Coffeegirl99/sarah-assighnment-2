92f68f9f750e80d70dd6f60707f531ec72d775bc
Sarah Lagares <smlagares99@gmail.com>
2023-04-14 16:00:22 -0400

    4/14 commit

--- src/com/coderscampus/JobService.java
+++ src/com/coderscampus/JobService.java
@@ -1,6 +1,7 @@
 package com.coderscampus;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,20 +9,52 @@ import java.util.List;
 
 public class JobService {
 
-	private Jobs createJobs (String username, String password) {
-		Jobs jobs = new Jobs(password, password, password);
-		jobs.setUsername(username);
-		jobs.setPassword(password);
-		return jobs;
+	BufferedReader fileReader = null;
+	{
+		try {
+			fileReader = new BufferedReader(new FileReader("jobs.txt"));
+			// Split each line into different places
+			String line;
+
+			while ((line = fileReader.readLine()) != null) {
+				String[] jobsArray = line.split("\",\"");
+				for (String jobs : jobsArray) {
+					System.out.println(jobs);
+				}
+				// Store each piece into an array for each line
+
+				String title = jobsArray[0];
+				String company = jobsArray[1];
+				String compensation = jobsArray[2];
+				
+//Assign each item in the array into variables such as title, company, compensation	
+//Need to 				
+				
+			}
+		} catch (
+
+		FileNotFoundException e) {
+			System.out.println("There was an exception");
+
+		} catch (IOException e) {
+			System.out.println("There was an IO Exeption");
+
+		} finally {
+			try {
+				fileReader.close();
+			} catch (IOException e) {
+				System.out.println("There was closing IO exception");
+			}
+
+		}
 	}
 
-	public List<Jobs> filteredJobs(List<Jobs> jobs) {
-		List<Jobs> filteredJobs = new ArrayList<>();
-		for (Jobs job : jobs) {
-			if (job.getCompensation() != null) {
-				filteredJobs.add(job);
-			}
-		}
-		return filteredJobs;
+//Create a method to create a user JOBSERVICE using those variables
+	public Jobs createJobs(String title, String company, String compensation) {
+		Jobs jobs = new Jobs();
+		jobs.setTitle(title);
+		jobs.setCompany(company);
+		jobs.setCompensation(compensation);
+		return jobs;
 	}
 }
--- src/com/coderscampus/Jobs.java
+++ src/com/coderscampus/Jobs.java
@@ -6,27 +6,32 @@ public class Jobs {
 	private String company;
 	private String compensation;
 
-	public Jobs(String title, String company, String compensation) {
-		this.title = title;
-		this.company = company;
-		this.compensation = compensation;
-	}
-
 	public String getTitle() {
 		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public String getCompany() {
 		return company;
 	}
 
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
 	public String getCompensation() {
 		return compensation;
 	}
 
+	public void setCompensation(String compensation) {
+		this.compensation = compensation;
+	}
+
 	@Override
 	public String toString() {
-		return title + "/n" + company + "/n" + compensation + "/n";
+		return title + company + compensation;
 	}
 }
-//Jobs "[title + "/n" + company + "/n" + compensation + "/n"]"
